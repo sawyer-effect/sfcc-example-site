4426b72242dde5efd022ca194ec8bc29962accd9
SantiagoH3 <35846982+SantiagoH3@users.noreply.github.com>
2019-07-02 16:57:16 -0600

    Create cartridge for cybersource.
    * First steps for cybersource cartridge.
    * Adding necessary files for SFRA cybersource.
    * Adding cartridge to JSON file.

---
+++ cartridges/LINK_cybersource/cartridge/client/default/js/checkout.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var processInclude = require('base/util');
+
+$(document).ready(function () {
+    processInclude(require('./checkout/checkout'));
+    processInclude(require('./checkout/cybersource/secureAcceptance'));
+    processInclude(require('./checkout/cybersource/flexMicroform'));
+    processInclude(require('./checkout/shippingDAV'));
+});
