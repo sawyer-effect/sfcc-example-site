bf9a24be3c8283695b4d7cf6691aa39e9b462be0
SantiagoH3 <35846982+SantiagoH3@users.noreply.github.com>
2019-07-02 11:13:16 -0600

    First Controller and islot.
    * Create controller for new Page.
    * Registers the Show and ErrorNotFound for the Page module.
    * Closing input in homePage.isml

---
+++ cartridges/app_custom_example/cartridge/controllers/Page.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var server = require('server');
+var cache = require('*/cartridge/scripts/middleware/cache');
+
+server.get('Show', cache.applyDefaultCache, function (req, res, next) {
+    res.render('/page/pageTest');
+    next();
+});
+
+server.get('ErrorNotFound', function (req, res, next) {
+    res.setStatusCode(404);
+    res.render('error/notFound');
+    next();
+});
+
+module.exports = server.exports();
